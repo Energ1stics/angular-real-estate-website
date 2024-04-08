3948bf3ea096938bc10e0cbe1f2ecc0f7ebb171a
Paul Hebst <p.hebst@intigoo.de>
2024-04-08 15:36:46 +0200

    Add basic pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'property', component: PageNotFoundComponent },
+  { path: 'about', component: PageNotFoundComponent },
+  { path: 'service', component: PageNotFoundComponent },
+  { path: 'contact', component: PageNotFoundComponent },
+];
