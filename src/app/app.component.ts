4bbe23e1b00476a0695aea5acf458335faec0801
Paul Hebst <p.hebst@intigoo.de>
2024-04-11 15:37:31 +0200

    Remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import Clerk from '@clerk/clerk-js';
 import { environment } from '../environments/environment';
-import { User } from '@clerk/clerk-js/dist/types/core/resources/User';
 
 @Component({
   selector: 'app-root',
@@ -19,9 +18,5 @@ export class AppComponent {
 
   async ngOnInit() {
     await this.clerk.load();
-  }
-
-  onSignInButtonClick() {
-    console.log(this.clerk.user);
   }
 }
