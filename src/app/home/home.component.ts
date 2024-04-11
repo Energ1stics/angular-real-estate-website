8c80fedc35bec21e5bfb54edfaa5a6157bc7f5b9
Paul Hebst <p.hebst@intigoo.de>
2024-04-11 19:53:46 +0200

    Add simple tabs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  openTab = 0;
 
+  toggleTab(index: number) {
+    console.log('Change from tab ', this.openTab, ' to tab ', index);
+    this.openTab = index;
+  }
 }
